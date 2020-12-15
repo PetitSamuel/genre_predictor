8f0b66d6c5b7f7b020220e2d62836293b424b506
MySupersuit <bobtom8482gm@gmail.com>
2020-12-15 11:52:56 +0000

    added explicit cross validation + graphs

--- prediction_model.py
+++ prediction_model.py
@@ -34,7 +34,7 @@ def main():
             data[i]['acousticness'],
             data[i]['instrumentalness'],
             data[i]['liveness'],
-            data[i]['valence']
+            data[i]['valence'],
         ]
         x.append(features)
         y.append(labels)
@@ -42,12 +42,84 @@ def main():
     y = np.array(y)
     x = np.array(x)
 
+    from sklearn.utils import shuffle
+    x, y = shuffle(x, y)
+    x_train = x[:10000]
+    y_train = y[:10000]
+    x_hold_out = x[10000:]
+    y_hold_out = y[10000:]
+    print(len(x_hold_out))
+
     # train and test each model - accuracy %
     # knn(x, y)               # 40%
     # decision_tree(x, y)  # 56%
-    random_forest(x, y)     # 61%
+    # random_forest(x, y)     # 61+%
     # baseline(x,y)           # 25%
     # logistic_reg(x,y.ravel()) # 40%
+    # lr_cv_q(x, y)
+    # lr_cv_C(x,y)
+    # knn_cv_n_neighbours(x,y,[1,3,5])
+    # knn_cv_gamma(x,y,[0.001,0.1,1]) # 52%
+
+    knn_hold_out(x_train,y_train,x_hold_out,y_hold_out)
+
+
+def lr_cv_q(x, y):
+    from sklearn.utils import shuffle
+    x, y, = shuffle(x, y)
+
+    mean_error = []
+    std_error = []
+    q_range = [1, 2, 3]  # seems q=2 is slightly better than q=1
+    for q in q_range:
+        from sklearn.preprocessing import PolynomialFeatures
+        xpoly = PolynomialFeatures(q).fit_transform(x)
+
+        from sklearn.linear_model import LogisticRegression
+        model = LogisticRegression(multi_class='ovr', solver='liblinear', C=100)
+        temp = []
+
+        from sklearn.model_selection import KFold
+        kf = KFold(n_splits=5)
+        for train, test in kf.split(xpoly):
+            model.fit(xpoly[train], y[train].ravel())
+
+            score = model.score(xpoly[test], y[test])
+            print(score)
+            temp.append(score)
+        mean_error.append(np.array(temp).mean())
+        std_error.append(np.array(temp).std())
+    plt.errorbar(q_range, mean_error, yerr=std_error, elinewidth=3)
+    plt.xlabel('q')
+    plt.ylabel('Mean accuracy')
+    plt.show()
+
+
+def lr_cv_C(x, y):
+    mean_error = []
+    std_error = []
+    C_range = [5, 8, 10, 20, 50]
+    for C in C_range:
+
+        from sklearn.linear_model import LogisticRegression
+        model = LogisticRegression(multi_class='ovr', solver='liblinear', C=C)
+        temp = []
+        plotted = False
+
+        from sklearn.model_selection import KFold
+        kf = KFold(n_splits=5)
+        for train, test in kf.split(x):
+            model.fit(x[train], y[train])
+
+            score = model.score(x[test], y[test])
+            print(score)
+            temp.append(score)
+        mean_error.append(np.array(temp).mean())
+        std_error.append(np.array(temp).std())
+    plt.errorbar(C_range, mean_error, yerr=std_error, elinewidth=3)
+    plt.xlabel('C')
+    plt.ylabel('Mean accuracy')
+    plt.show()
 
 
 def logistic_reg(x, y):
@@ -55,8 +127,21 @@ def logistic_reg(x, y):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
     from sklearn.linear_model import LogisticRegression
-    clf_lr = LogisticRegression(multi_class='ovr', solver='liblinear')
-    clf_lr.fit(x_train, y_train)
+    lr_pipeline = Pipeline([
+        ('lr', LogisticRegression(multi_class='ovr', solver='liblinear'))
+    ])
+
+    lr_params = {
+        'lr__C': [1000, 100, 10]
+    }
+
+    clf_lr = GridSearchCV(lr_pipeline, lr_params)
+
+    # fit on the best hyperparameters
+    clf_lr.fit(x_train, y_train.ravel())
+
+    print("Best Score: ", clf_lr.best_score_)
+    print("Best Params: ", clf_lr.best_params_)
 
     print(clf_lr.score(x_test, y_test))
 
@@ -73,6 +158,77 @@ def baseline(x, y):
 
     from sklearn.metrics import classification_report
     print(classification_report(y_pred, y))
+
+
+def create_gaussian_kernel(gamma):
+    def g(distances):
+        weights = np.exp(-gamma * (distances ** 2))
+        return weights / np.sum(weights)
+
+    return g
+
+
+def knn_cv_n_neighbours(x, y, neighbours):
+    mean_error = []
+    std_error = []
+
+    for neighbour in neighbours:
+        # kernel = create_gaussian_kernel(gamma)
+        from sklearn.neighbors import KNeighborsClassifier
+        # num_neighbours = int(len(x) * 0.8)
+        model = KNeighborsClassifier(n_neighbors=neighbour)
+
+        temp = []
+        from sklearn.model_selection import KFold
+        kf = KFold(n_splits=5)
+        for train, test in kf.split(x):
+            model.fit(x[train], y[train])
+
+            score = model.score(x[test], y[test])
+            print(score)
+            temp.append(score)
+        mean_error.append(np.array(temp).mean())
+        std_error.append(np.array(temp).std())
+    plt.errorbar(neighbours, mean_error, yerr=std_error)
+    plt.title("#Neighbours vs Mean Accuracy with standard error")
+    plt.xlabel("# Neighbours")
+    plt.ylabel("Mean accuracy")
+    plt.show()
+
+
+def knn_cv_gamma(x, y, gammas):
+    mean_error = []
+    std_error = []
+
+    for g in gammas:
+        kernel = create_gaussian_kernel(g)
+        from sklearn.neighbors import KNeighborsClassifier
+        # num_neighbours = int(len(x) * 0.8)
+        model = KNeighborsClassifier(n_neighbors=1, weights=kernel)
+
+        temp = []
+        from sklearn.model_selection import KFold
+        kf = KFold(n_splits=10)
+        for train, test in kf.split(x):
+            model.fit(x[train], y[train])
+
+            score = model.score(x[test], y[test])
+            print(score)
+            temp.append(score)
+        mean_error.append(np.array(temp).mean())
+        std_error.append(np.array(temp).std())
+    plt.errorbar(gammas, mean_error, yerr=std_error)
+    plt.title("gamma vs Mean Accuracy with standard error")
+    plt.xlabel("gamma")
+    plt.ylabel("Mean accuracy")
+    plt.show()
+
+
+def knn_hold_out(x,y,x_hold,y_hold):
+    from sklearn.neighbors import KNeighborsClassifier
+    model = KNeighborsClassifier(n_neighbors=1)
+    model.fit(x,y)
+    print("hold-out score", model.score(x_hold,y_hold))
 
 
 # knn network - number of neighbours cross validated
